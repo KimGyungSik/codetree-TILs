f1e6b9da01e2e38a3edc2ae3b0241e240b2c807d
KimGyungSik <139200972+KimGyungSik@users.noreply.github.com>
2024-01-20 15:43:07 +0900

    Title: 특정 조건 배수의 합, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240120/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
+++ 240120/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
@@ -7,11 +7,21 @@ public class Main {
         int b = s.nextInt();
         int sum = 0;
 
+        if(a<b) {
         for (int i = a; i <= b; i++) {
             if (i % 5 ==0) {
                 sum += i;
+                }
             }
+        }else {
+            for (int i = b; i <= a; i++) {
+            if (i % 5 ==0) {
+                sum += i;
+                }
+            }
+
         }
+
         System.out.println(sum);
     }
 }
