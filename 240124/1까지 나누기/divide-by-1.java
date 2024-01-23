24311bf7d3b9fa4376f359956977eb597bd1ed5f
KimGyungSik <139200972+KimGyungSik@users.noreply.github.com>
2024-01-24 01:20:30 +0900

    Title: 1까지 나누기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240124/1까지 나누기/divide-by-1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        // 나눗셈을 진행한 총 횟수를 담을 변수
+        int answer = 0;
+        // 나눗셈을 시작할 숫자
+        int i = 1;
+        // 몫이 1이하가 되는 순간 반복문 종료
+        while(n>=1) {
+            n = n/i++;
+            answer++;
+        }
+        System.out.println(answer);
+    }
+}
