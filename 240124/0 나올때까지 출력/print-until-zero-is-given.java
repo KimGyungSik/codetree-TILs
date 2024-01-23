c633f6acc657dfacd6314e710f747e4acb27f345
KimGyungSik <139200972+KimGyungSik@users.noreply.github.com>
2024-01-24 01:36:56 +0900

    Title: 0 나올때까지 출력, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240124/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        while(true) {
+            int a = s.nextInt();
+            if(a==0)
+                break;
+            else 
+                System.out.println(a);
+        }
+    }
+}
